9c24811f0ca474b5395cf7b5c0a9892ff2951c23
liushoukai <21156929@qq.com>
2015-11-20 18:53:33 +0800

    change sytle

--- static/js/post.js
+++ static/js/post.js
@@ -8,7 +8,7 @@
 $(document).ready(function() {
     generateContent();
     share();
-    disqus();
+    //disqus();
 });
 
 /**
@@ -23,8 +23,22 @@ function generateContent() {
     });
 }
 
+/**
+ * 百度分享
+ */
 function share(){
-    window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};
+    window._bd_share_config={
+        "common":{
+            "bdSnsKey": {},
+            "bdText": "",
+            "bdMini": "2",
+            "bdMiniList": false,
+            "bdPic": "",
+            "bdStyle": "1",
+            "bdSize": "24"
+        },
+        "share":{}
+    };
     with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
 }
 
